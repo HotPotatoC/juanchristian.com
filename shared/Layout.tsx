6571cd3aa59dcfaf9d159832aaff5432130f7191
HotPotatoC <juandotulung@gmail.com>
2021-09-17 20:00:46 +0700

    fix(seo): updated SEO urls

--- shared/Layout.tsx
+++ shared/Layout.tsx
@@ -19,18 +19,18 @@ const Layout: React.FC<LayoutProps> = ({ children, title, description }) => {
           title,
           description,
           type: "website",
-          url: "https://juanchristian.com",
+          url: "https://www.juanchristian.com",
           locale: "en_US",
           images: [
             {
-              url: "https://juanchristian.com/images/me2.png",
+              url: "https://www.juanchristian.com/images/me2.png",
               width: 1080,
               height: 1080,
               alt: "Me",
             },
           ],
         }}
-        canonical='https://juanchristian.com'
+        canonical='https://www.juanchristian.com'
         twitter={{
           handle: "@juanwmv",
           site: "@juanwmv",
