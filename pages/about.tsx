4a036eef74372584bd6416656e53d1d73685d613
HotPotatoC <juandotulung@gmail.com>
2023-04-09 19:31:27 +0700

    refactor: changed page export format

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,16 @@
-import type { NextPage } from 'next'
 import Layout from '../shared/Layout'
 
 import { Introduction } from '../modules/about/Introduction'
 import Skills from '../modules/about/Skills'
 
-const About: NextPage = () => (
-  <Layout
-    title="Juan Christian — Indonesian Developer"
-    description="Juan Christian is a Developer based in Jakarta, Indonesia."
-  >
-    <Introduction />
-    <Skills />
-  </Layout>
-)
-
-export default About
+export default function About() {
+  return (
+    <Layout
+      title="Juan Christian — Indonesian Developer"
+      description="Juan Christian is a Developer based in Jakarta, Indonesia."
+    >
+      <Introduction />
+      <Skills />
+    </Layout>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
-import type { NextPage } from 'next'
-
 import Hero from '../modules/home/Hero'
 import { Portfolio } from '../modules/home/Portfolio'
 import Skills from '../modules/home/Skills'
 import Layout from '../shared/Layout'
 
-const Home: NextPage = () => (
-  <Layout
-    title="Juan Christian — Indonesian Developer"
-    description="Juan Christian is a Developer based in Jakarta, Indonesia."
-  >
-    <Hero />
-    <Portfolio />
-    <Skills />
-  </Layout>
-)
-
-export default Home
+export default function Home() {
+  return (
+    <Layout
+      title="Juan Christian — Indonesian Developer"
+      description="Juan Christian is a Developer based in Jakarta, Indonesia."
+    >
+      <Hero />
+      <Portfolio />
+      <Skills />
+    </Layout>
+  )
+}
