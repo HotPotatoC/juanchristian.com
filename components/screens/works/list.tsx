91fdff0ef57863cc19638de15dd3f6382546302a
HotPotatoC <juandotulung@gmail.com>
2023-08-21 23:50:07 +0700

    fix: overdelayed work item

--- components/screens/works/list.tsx
+++ components/screens/works/list.tsx
@@ -7,8 +7,8 @@ const WorksList = () => {
   return (
     <ContentWrapper extraClass="px-6 md:px-12 pt-12">
       <div className="flex flex-col space-y-4">
-        {works.map((work, idx) => (
-          <SlideUp key={work.path} delay={idx * 0.1}>
+        {works.map((work) => (
+          <SlideUp key={work.path} delay={0.2}>
             <WorkItem work={work} />
           </SlideUp>
         ))}
