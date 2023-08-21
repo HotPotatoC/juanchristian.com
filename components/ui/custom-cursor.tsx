883bf9a784ca78d93291823d0d9ff09c1628b947
HotPotatoC <juandotulung@gmail.com>
2023-08-21 14:22:17 +0700

    feat: added cursor arrow animation

--- components/ui/custom-cursor.tsx
+++ components/ui/custom-cursor.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { useEventListener } from '../../hooks/useEventListener'
 import useMousePosition from '../../hooks/useMousePosition'
-import { useTransition } from '../../lib/animation'
+import { expoEaseInOut, useTransition } from '../../lib/animation'
 import cn from '../../lib/cn'
 
 const CustomCursor = () => {
@@ -72,7 +72,7 @@ const CustomCursor = () => {
   return (
     <div
       className={cn(
-        'opacity-0 pointer-events-none fixed z-[999] transform -translate-x-1/2 -translate-y-1/2 p-2 hidden lg:block bg-white-100 rounded-full mix-blend-difference [transition:opacity_300ms,transform_400ms] ease-[cubic-bezier(0.19,1,0.22,1)]',
+        'overflow-hidden opacity-0 pointer-events-none fixed z-[999] transform -translate-x-1/2 -translate-y-1/2 p-2 hidden lg:block bg-white-100 rounded-full mix-blend-difference [transition:opacity_300ms,transform_400ms] ease-[cubic-bezier(0.19,1,0.22,1)]',
         isActive && ['opacity-1', isActiveOnLink ? 'scale-[7]' : 'scale-[5]'],
         isClicked && 'scale-[3]'
       )}
@@ -87,34 +87,46 @@ const CustomCursor = () => {
               left: '50%',
               translateX: '-50%',
               translateY: '-50%',
-              rotate: -90,
+              opacity: 0,
             }}
             animate={{
               top: '50%',
               left: '50%',
               translateX: '-50%',
               translateY: '-50%',
-              rotate: 0,
+              opacity: 1,
             }}
             exit={{
               top: '50%',
               left: '50%',
               translateX: '-50%',
               translateY: '-50%',
-              rotate: -90,
+              opacity: 0,
             }}
             transition={transition}
           >
-            <svg
-              viewBox="0 0 22 22"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+            <motion.div
+              initial={{ x: -12 }}
+              animate={{ x: 12 }}
+              transition={{
+                repeat: Infinity,
+                repeatType: 'loop',
+                duration: 1.25,
+                ease: expoEaseInOut,
+              }}
             >
-              <path
-                d="M16.5627 9.66673L9.41071 2.51473L11.296 0.629395L21.6667 11.0001L11.296 21.3707L9.41071 19.4854L16.5627 12.3334H0.333374V9.66673H16.5627Z"
-                fill="currentColor"
-              />
-            </svg>
+              <svg
+                viewBox="0 0 22 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="scale-125"
+              >
+                <path
+                  d="M16.5627 9.66673L9.41071 2.51473L11.296 0.629395L21.6667 11.0001L11.296 21.3707L9.41071 19.4854L16.5627 12.3334H0.333374V9.66673H16.5627Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
